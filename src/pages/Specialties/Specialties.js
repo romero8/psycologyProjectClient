ff356848c6a7db161fc04a2f08dc09264a5acde2
romero8 <101278601+romero8@users.noreply.github.com>
2023-05-22 15:59:04 +0300

    Add files via upload

--- src/pages/Specialties/Specialties.js
+++ src/pages/Specialties/Specialties.js
@@ -4,7 +4,7 @@ import logo from "../../icons/AnimatedLogo.png";
 import { MainBtn } from "../../components/MainBtn/MainBtn";
 import { faLocationDot, faVideo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { therapistTypesData,allUsers } from "../../helpers/data";
+import { therapistTypesData, allUsers } from "../../helpers/data";
 import { Link } from "react-router-dom";
 
 export function Specialties(props) {
@@ -18,9 +18,15 @@ export function Specialties(props) {
   let { gender } = useParams();
   let { lgbtq } = useParams();
 
-  let therapistInfoArr = [specialty,name, profession, city, language, experience, gender];
-  console.log(therapistInfoArr)
-  console.log(therapistInfoArr.map(therapist => therapist))
+  let therapistInfoArr = [
+    specialty,
+    profession,
+    city,
+    language,
+    experience,
+    gender,
+  ];
+
   return (
     <div className="cardsContainer">
       <div className="cardsTitle">
@@ -35,51 +41,60 @@ export function Specialties(props) {
       </div>
       {allUsers.map((user) => {
         if (user.typeName === specialty) {
-            return (
-              <div className="cardContainer">
-                <div className="cardPhotoBox">
-                  <img className="img" src={logo} />
-                </div>
-                <div className="cardInfo">
-                  <Link
-                    className="specalistName"
-                    to={`/specialties/${specialty}/${user.name}`}
-                  >
-                    <h3>{user.name}</h3>
-                  </Link>
-                  <span className="specalistAbility">{specialty}</span>
-                  <span className="specalistAbout">{`Best ${specialty} Ever`}</span>
-                  <div className="specalistAvailabilityBox">
-                    <div className="iconBox">
-                      <FontAwesomeIcon
-                        icon={faLocationDot}
-                        className="cardIcon"
-                      />
-                    </div>
-                    <p className="specalistAvailability">{user.address.city}</p>
+          return (
+            <div className="cardContainer">
+              <div className="cardPhotoBox">
+                <img className="img" src={logo} />
+              </div>
+              <div className="cardInfo">
+                <Link
+                  className="specalistName"
+                  to={`/specialties/${specialty}/${user.name}`}
+                >
+                  <h3>{user.name}</h3>
+                </Link>
+                <span className="specalistAbility">{specialty}</span>
+                <span className="specalistAbout">{`Best ${specialty} Ever`}</span>
+                <div className="specalistAvailabilityBox">
+                  <div className="iconBox">
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      className="cardIcon"
+                    />
                   </div>
-                  <div className="specalistAvailabilityBox">
-                    <div className="iconBox">
-                      <FontAwesomeIcon icon={faVideo} className="cardIcon" />{" "}
-                    </div>
-                    <p className="specalistAvailability">Video Call</p>
-                  </div>
+                  <p className="specalistAvailability">{user.address.city}</p>
                 </div>
-                <div className="cardActions">
-                  <MainBtn value="Call" />
-                  <MainBtn value="Appointment" />
+                <div className="specalistAvailabilityBox">
+                  <div className="iconBox">
+                    <FontAwesomeIcon icon={faVideo} className="cardIcon" />{" "}
+                  </div>
+                  <p className="specalistAvailability">Video Call</p>
                 </div>
               </div>
-            );
-         
+              <div className="cardActions">
+                <MainBtn value="Call" />
+                <MainBtn value="Appointment" />
+              </div>
+            </div>
+          );
         }
-        return therapistInfoArr.map((therapist) => {
-          console.log(therapist)
-          if (user.name || user.adress.city || user.typeName || user.experience || user.gender === therapist){
-            return <h1>{therapist}</h1>
+
+        return allUsers.map((user) => {
+          let therapistInfoObj = [
+            { index: specialty, id: user.name },
+            { index: profession, id: user.typeName },
+            { index: city, id: user.address.city },
+            { index: language, id: user.language },
+            { index: experience, id: user.experience },
+            { index: gender, id: user.gender },
+          ];
+
+          for(let i = 0; i < therapistInfoObj.length; i++)
+          if(therapistInfoObj[i].index==='all'){
+            console.log(therapistInfoObj[i].id)
+            return <h1>{therapistInfoObj[i].id}</h1>
           }
-        })
-       
+        });
 
         // if (therapistType.typeName === specialty) {
         //   return therapistType.users.map((user) => {
